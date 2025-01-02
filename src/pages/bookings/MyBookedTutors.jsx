0de3d09c736d824b4a5c0392ea2c80d9718c0fa3
FaisalHasanEmon <faisalhasan5833@gmail.com>
2025-01-02 06:48:19 +0600

    My Booked Tutorials Added

--- src/pages/bookings/MyBookedTutors.jsx
+++ src/pages/bookings/MyBookedTutors.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import FindTutorsCards from "../../components/FindTutorsCards";
+import axios from "axios";
+import UseAuth from "../../context/UseAuth";
+import serverDomain from "../../api/serdomain";
 
 const MyBookedTutors = () => {
-  return <div>This is My Booked Tutors Page</div>;
+  const { user } = UseAuth();
+  const [myBookings, setMyBookings] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${serverDomain}/bookedTutorials/${user?.email}`)
+      .then((res) => setMyBookings(res?.data));
+  }, [user]);
+  const cameFrom = "myBookings";
+  console.log(myBookings);
+  const handleDelete = () => {
+    console.log("Item Deleted");
+  };
+  return (
+    <div>
+      <div>
+        <p className="font-bold">
+          You've Booked : {myBookings?.length ? myBookings.length : 0}
+          Tutorials
+        </p>
+      </div>
+      <div className="grid gird-cols-1 md:grid-cols-3 gap-12">
+        {myBookings.map((tutor) => (
+          <FindTutorsCards
+            key={tutor._id}
+            tutor={tutor}
+            cameFrom={cameFrom}
+            handleDelete={handleDelete}
+          ></FindTutorsCards>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyBookedTutors;
